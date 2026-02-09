ae16d311bd215e32067f115d64f94d95c8a5b9a9
Amal Raj <amalraj2821@gmail.com>
2026-02-09 20:20:13 +0530

    Organized the sequences into a separate file

---
+++ sequences.py
@@ -0,0 +1,104 @@
+from qiskit import QuantumCircuit
+from qiskit.circuit.library import HGate, XGate, YGate, ZGate, SGate, SdgGate, TGate, TdgGate
+
+inverse_pairs = {
+    "h": [HGate, HGate],
+    "t": [TGate, TdgGate],
+    "s": [SGate, SdgGate],
+    "tdg": [TdgGate, TGate],
+    "sdg": [SdgGate, SGate],
+    "x": [XGate, XGate],
+    "y": [YGate, YGate],
+    "z": [ZGate, ZGate],    
+}
+
+
+cloaked_gates_sequences = {
+    "x": [
+        ["h", "z", "h"],
+        ["s", "y", "s", "z"],
+        ["z", "sdg", "y", "sdg"],
+        ["h", "y", "z", "h", "y"],
+        ["y", "h", "z", "y", "h"],
+        ["h", "y", "h","y", "sdg", "y", "s"],
+        ["sdg", "y", "s", "h", "y", "h", "y"],
+        ["s", "y", "sdg"],
+    ],
+    "y": [
+        ["sdg", "x", "s"],
+        ["sdg", "x", "z", "sdg"],
+        ["s", "z", "x", "s"],
+    ],
+    "z": [
+        ["h", "x", "h"],
+        ["x", "s", "y", "s"],
+        ["sdg", "y", "sdg", "x"],
+        ["y", "h", "x", "y", "h"],
+        ["h", "y", "x", "h", "y"],
+        ["s", "s"],
+        ["t", "t", "t", "t"],
+    ],
+    "s": [
+        ["t", "t"],
+        ["z", "t", "z", "t"],
+        ["tdg", "tdg", "z"]
+    ],
+}
+
+
+delayed_gates_sequences = {
+    "x": [
+        ["h", "z", "x", "h", "z"],
+        ["h", "y", "h", "y", "z", "x", "z"],
+        ["h", "y", "h", "x", "y"],
+        ["y", "x", "h", "y", "h"],
+        ["z", "h", "x", "z", "h"],
+        ["z", "x", "z", "y", "h", "y", "h"],
+    ],
+    "y": [["x", "h", "y", "h", "x"]],
+    "z": [
+        ["h", "y", "h", "z", "y"],
+        ["y", "z", "h", "y", "h"],
+        ["h", "y", "h", "y", "x", "z", "x"],
+        ["x", "z", "x", "y", "h", "y", "h"],
+        ["x", "h", "z", "x", "h"],
+        ["h", "x", "z", "h", "x"],
+        ["sdg", "z", "s"],
+        ["s", "z", "sdg"],
+    ],
+    "h": [["x", "z", "h", "x", "z"], ["z", "x", "h", "z", "x"]],
+    "s": [
+        ["z", "t", "sdg", "t"],
+        ["z", "tdg", "s", "t", "z"],
+        ["h", "y", "h", "s", "x"],
+    ],
+    "t": [["z", "sdg", "t", "s", "z"], ["z", "s", "t", "sdg", "z"]],
+}
+
+
+def auxiliary_gate():
+    sub_circuit = QuantumCircuit(1, name="auxiliary")
+    sub_circuit.h(0)
+    sub_circuit.h(0)
+    sub_circuit.z(0)
+    sub_circuit.x(0)
+    sub_circuit.z(0)
+    sub_circuit.x(0)
+    auxiliary = sub_circuit.to_gate()
+    auxiliary.name = "auxiliary"
+    return auxiliary
+
+
+def restore_gate():
+    sub_circuit = QuantumCircuit(1, name="restore")
+    sub_circuit.x(0)
+    sub_circuit.z(0)
+    sub_circuit.x(0)
+    sub_circuit.z(0)
+    sub_circuit.h(0)
+    sub_circuit.h(0)
+    restore = sub_circuit.to_gate()
+    restore.name = "restore"
+    return restore
+
+compositeGateSequences = {"aux": auxiliary_gate(), "res": restore_gate()}
